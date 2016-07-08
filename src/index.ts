ce468e348a3eb1b4bde22ae8246e5b470bae66de
Louis Orleans <louis@orleans.io>
2016-07-08 14:59:15 -0700

    Starting on layout

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export function MethodName (parameters): string {
-    return 'Hello, World!';
+import {log, PluginError, File} from 'gulp-util';
+import * as through from 'through2';
+
+export interface IForemanConfig {
+    cwd: string;
+    procFile: string;
+    envFile: string;
+    port: number;
+};
+
+export function Foreman (config: IForemanConfig): void | File {
+    return;
 };
